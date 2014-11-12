75660db2794c87a30a9e1112c7d1458395890e7b
yangboz <YoungWelle@gmail.com>
2014-11-12 14:35:25 +0800

    CHANGED:PersonCreate dto and test json file

--- spring-boot/rest-octo-ninja/src/main/java/constellation/PersonController.java
+++ spring-boot/rest-octo-ninja/src/main/java/constellation/PersonController.java
@@ -2,6 +2,8 @@ package constellation;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.StringReader;
+import java.math.BigInteger;
 import java.net.URL;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
@@ -14,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.wordnik.swagger.annotations.ApiOperation;
 
-import constellation.dto.Person;
+import constellation.dto.PersonCreate;
 
 import com.facepp.error.FaceppParseException;
 import com.facepp.http.HttpRequests;
@@ -26,62 +28,66 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import constellation.ApiConsts;
 
+import java.security.SecureRandom;
+
+
 @RestController
 public class PersonController {
-	@RequestMapping(method=RequestMethod.GET,value="constellation/person")
-	@ApiOperation(httpMethod="GET",value="Retrieve information of a person, including name, id, tag, faces assigned to the person, and groups that the person belongs to.")
-	public Person get() throws Exception
+	//
+	private SecureRandom random = new SecureRandom();
+	public String nextSessionId(){
+		return new BigInteger(130,random).toString(32);
+	}
+	
+	@RequestMapping(method=RequestMethod.GET,value="constellation/person/create")
+	@ApiOperation(httpMethod="GET",value="Create a person.One Person may contain at most 10000 Faces,The app under developer status can  NOT create more than 100 persons.")
+	public PersonCreate create() throws Exception
 	{
 		ObjectMapper mapper = new ObjectMapper();
-		Person person = null;
-		// Load the directory as a resource
-		URL dir_url = ClassLoader.getSystemResource("person.json");
-		person = mapper.readValue(new File(dir_url.toURI()), Person.class);
-		return person;
-		//
+		PersonCreate personCreate = null;
 		/*
+		// Load the directory as a resource　for testing.
+		URL dir_url = ClassLoader.getSystemResource("PersonCreate.json");
+		personCreate = mapper.readValue(new File(dir_url.toURI()), PersonCreate.class);
+		return personCreate;
+		*/
+		//
 		HttpRequests httpRequests = new HttpRequests(ApiConsts.API_KEY_FACEPP, ApiConsts.API_SECRET_FACEPP, true, true);
-		
 		JSONObject result = null;
-		
+//		
 		try {
-			
-			System.out.println(Charset.forName("UTF-8").name());
-		
+//			System.out.println(Charset.forName("UTF-8").name());
 			System.out.println("FacePlusPlus API Test:");
-			
 			//detection/detect
 			result = httpRequests.detectionDetect(new PostParameters().setUrl("http://cn.faceplusplus.com/wp-content/themes/faceplusplus/assets/img/demo/9.jpg"));
 			System.out.println(result);
-			
 			System.out.println(result.getJSONArray("face").getJSONObject(0).getJSONObject("position").getJSONObject("center"));
-			
 			//-----------------Person-----------------
 			//person/create
 			System.out.println("\nperson/create");
 //			for (int i = 0; i < result.getJSONArray("face").length(); ++i)
 			for (int i = 0; i < 1; ++i)	
 			{
-				person = mapper.readValue(httpRequests.personCreate(new PostParameters().setPersonName("person_"+i)).toString(), Person.class);
-//				System.out.println(httpRequests.personCreate(new PostParameters().setPersonName("person_"+i)));
-				System.out.println(person);
+				JSONObject jsonObj = httpRequests.personCreate(new PostParameters().setPersonName("person_"+nextSessionId()));
+				System.out.println("jsonObj:"+jsonObj.toString());
+				personCreate = mapper.readValue(new StringReader(jsonObj.toString()), PersonCreate.class);
+//				System.out.println("personCreate:"+personCreate.toString());
 			}
 		} catch(FaceppParseException e) {
 			e.printStackTrace();
 		} catch (Exception e) {
 		} finally {
-			try {
-				for (int i = 1; i < result.getJSONArray("face").length(); ++i) {
-					httpRequests.personDelete(new PostParameters().setPersonName("person_"+i));
-					httpRequests.facesetDelete(new PostParameters().setFacesetName("faceset_"+i));
-				}
-			} catch (JSONException e) {
-				e.printStackTrace();
-			} catch (FaceppParseException e) {
-				e.printStackTrace();
-			}
+//			try {
+//				for (int i = 1; i < result.getJSONArray("face").length(); ++i) {
+//					httpRequests.personDelete(new PostParameters().setPersonName("person_"+i));
+//					httpRequests.facesetDelete(new PostParameters().setFacesetName("faceset_"+i));
+//				}
+//			} catch (JSONException e) {
+//				e.printStackTrace();
+//			} catch (FaceppParseException e) {
+//				e.printStackTrace();
+//			}
 		}
-		return person;
-		*/
+		return personCreate;
 	}
 }
---
+++ spring-boot/rest-octo-ninja/src/main/java/constellation/dto/PersonCreate.java
@@ -0,0 +1,48 @@
+package constellation.dto;
+
+import java.util.ArrayList;
+
+public class PersonCreate {
+	private int response_code;
+	public int getResponse_code() {
+		return response_code;
+	}
+	public void setResponse_code(int response_code) {
+		this.response_code = response_code;
+	}
+	private int added_face;
+	public int getAdded_face() {
+		return added_face;
+	}
+	public void setAdded_face(int added_face) {
+		this.added_face = added_face;
+	}
+	private int added_group;
+	public int getAdded_group() {
+		return added_group;
+	}
+	public void setAdded_group(int added_group) {
+		this.added_group = added_group;
+	}
+	private String tag;
+	public String getTag() {
+		return tag;
+	}
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+	private String person_name;
+	public String getPerson_name() {
+		return person_name;
+	}
+	public void setPerson_name(String person_name) {
+		this.person_name = person_name;
+	}
+	private String person_id;
+	public String getPerson_id() {
+		return person_id;
+	}
+	public void setPerson_id(String person_id) {
+		this.person_id = person_id;
+	}
+}
