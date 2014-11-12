377b3edc960d3bfd6b0ea8f32505738a6b7de552
yangboz <YoungWelle@gmail.com>
2014-11-12 20:30:01 +0800

    CHANGED:correct the pom.xml, try to using custom basePath

--- spring-boot/rest-octo-ninja/src/main/java/constellation/SwaggerConfig.java
+++ spring-boot/rest-octo-ninja/src/main/java/constellation/SwaggerConfig.java
@@ -2,18 +2,23 @@ package constellation;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 import com.mangofactory.swagger.configuration.SpringSwaggerConfig;
+import com.mangofactory.swagger.paths.AbsoluteSwaggerPathProvider;
+import com.mangofactory.swagger.paths.SwaggerPathProvider;
 import com.mangofactory.swagger.plugin.EnableSwagger;
 import com.mangofactory.swagger.plugin.SwaggerSpringMvcPlugin;
+import com.wordnik.swagger.config.ConfigFactory;
 import com.wordnik.swagger.model.ApiInfo;
 
 @Configuration
 @EnableSwagger
 public class SwaggerConfig {
 	private SpringSwaggerConfig springSwaggerConfig;
-
+	
 	   @Autowired
 	   public void setSpringSwaggerConfig(SpringSwaggerConfig springSwaggerConfig) {
 	      this.springSwaggerConfig = springSwaggerConfig;
@@ -21,8 +26,10 @@ public class SwaggerConfig {
 
 	   @Bean //Don't forget the @Bean annotation
 	   public SwaggerSpringMvcPlugin customImplementation(){
+//		  AbsoluteSwaggerPathProvider pathProvider = new AbsoluteSwaggerPathProvider();
 	      return new SwaggerSpringMvcPlugin(this.springSwaggerConfig)
 	            .apiInfo(apiInfo())
+//	            .pathProvider(pathProvider)
 	            .includePatterns("/constellation/.*");
 	   }
 
@@ -37,4 +44,5 @@ public class SwaggerConfig {
 	        );
 	      return apiInfo;
 	    }
+	    
 }
