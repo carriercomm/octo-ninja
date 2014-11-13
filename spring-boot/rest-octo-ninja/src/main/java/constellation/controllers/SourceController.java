578af4cd339fa83f3f192bef79316e2cc5dbc681
yangboz <YoungWelle@gmail.com>
2014-11-13 17:28:47 +0800

    ADDED:SourceController getFiles/Videos api function testing

--- spring-boot/rest-octo-ninja/src/main/java/constellation/controllers/SourceController.java
+++ spring-boot/rest-octo-ninja/src/main/java/constellation/controllers/SourceController.java
@@ -9,6 +9,13 @@ import com.wordnik.swagger.annotations.ApiOperation;
 
 import constellation.dto.PersonCreate;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import constellation.dto.SourceFile;
+
+import constellation.dto.SourceVideo;
+
 @RestController
 public class SourceController {
 	
@@ -17,8 +24,24 @@ public class SourceController {
 	public int create(@RequestParam(value="fid", required=false, defaultValue="0") int fileId,
 			@RequestParam(value="vid", required=false, defaultValue="0") int videoId) throws Exception
 	{
+		//TODO:save creating results to somewhere.
 		return fileId+videoId;
 	}
-	
+	@RequestMapping(method=RequestMethod.GET,value="constellation/source/files")
+	@ApiOperation(httpMethod="GET",value="Get list of files.")
+	public List<SourceFile> files(){
+		List<SourceFile> files = new ArrayList<SourceFile>();
+		//TODO:dynamic listing files.
+		files.add(new SourceFile());
+        return files;
+	}
+	@RequestMapping(method=RequestMethod.GET,value="constellation/source/videos")
+	@ApiOperation(httpMethod="GET",value="Get list of videos.")
+	public List<SourceVideo> videos(){
+		List<SourceVideo> videos = new ArrayList<SourceVideo>();
+		//TODO:dynamic listing videos.
+		videos.add(new SourceVideo());
+        return videos;
+	}
 	
 }
