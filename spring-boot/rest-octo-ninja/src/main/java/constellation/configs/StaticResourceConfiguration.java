9df5111417d237b4f2e39e1e11c7b603a14ff48f
yangboz <YoungWelle@gmail.com>
2015-04-27 23:09:32 +0800

    FIXED:static resource MVCConfiguration with pyhsic folder path configuation

--- spring-boot/rest-octo-ninja/src/main/java/constellation/configs/StaticResourceConfiguration.java
+++ spring-boot/rest-octo-ninja/src/main/java/constellation/configs/StaticResourceConfiguration.java
@@ -7,11 +7,28 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class StaticResourceConfiguration extends WebMvcConfigurerAdapter {
 	private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
-        "classpath:/META-INF/resources/", "classpath:/resources/",
-        "classpath:/static/", "classpath:/public/" };
+			"classpath:/META-INF/resources/", "classpath:/resources/",
+			"classpath:/static/", "classpath:/public/" };
+
+	/**
+	 * Add our static resources folder mapping.
+	 */
 	@Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
-        registry.addResourceHandler("/**").addResourceLocations("file:/Users/yangboz/Documents/Git/octo-ninja/spring-boot/rest-octo-ninja/src/main/resources/static");
-    }
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		//
+		registry.addResourceHandler("/**").addResourceLocations(
+				CLASSPATH_RESOURCE_LOCATIONS);
+		//
+		registry.addResourceHandler("/uploads/**").addResourceLocations(
+				"classpath:/uploads/");
+		// Activiti repository resources(diagram picture,process BPM files).
+		// registry.addResourceHandler("/repository/**").addResourceLocations("classpath:/repository/");
+		// Jasper report
+		registry.addResourceHandler("/static/**").addResourceLocations(
+				"classpath:/static/");
+		// registry.addResourceHandler("/reports/**").addResourceLocations("classpath:/reports/");
+		//
+		super.addResourceHandlers(registry);
+	}
+
 }
